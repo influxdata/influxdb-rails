3fdc1f3f13cede1262dea06365fc6329f48b01aa
Todd Persen <todd.persen@gmail.com>
2014-02-04 17:10:29 -0500

    Update spec_helper requirements.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,23 +3,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 ENV["RAILS_ENV"] ||= "test"
 
 require 'rails/version'
+require 'rails'
 
-if Rails::VERSION::MAJOR > 2
-  require 'rails'
-else
-  module Rails
-    class << self
-      def vendor_rails?; return false; end
-    end
-
-    class Configuration
-      def after_initialize; end
-    end
-
-    @@configuration = Configuration.new
-  end
-
-  require 'initializer'
+unless Rails::VERSION::MAJOR > 2
+  raise "Sorry, influxdb-rails only supports Rails 3.x and higher."
 end
 
 require 'bundler/setup'
@@ -32,11 +19,7 @@ if defined? Rails
   puts "Loading Rails v#{Rails.version}..."
 
   if Rails.version.to_f < 3.0
-    Gem::Deprecate.skip = true
-
-    RAILS_ROOT = "#{File.dirname(__FILE__)}/support/rails2"
-    require "#{RAILS_ROOT}/config/environment"
-    require "spec/rails"
+    raise "Sorry, Rails v#{Rails.version} isn't supported. Try upgrading to v3.0.0 or higher"
   elsif Rails.version.to_f < 4.0
     require "support/rails3/app"
     require "rspec/rails"
