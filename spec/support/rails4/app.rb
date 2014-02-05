551d7aa178fdb94b32a68bd43cef7497448129bf
Todd Persen <todd.persen@gmail.com>
2014-02-05 16:45:20 -0500

    Don't use old configuration stuff for Rails 4.

--- spec/support/rails4/app.rb
+++ spec/support/rails4/app.rb
@@ -14,8 +14,6 @@ app.routes.draw do
 end
 
 InfluxDB::Rails.configure do |config|
-  config.api_key = "f123-e456-d789c012"
-  config.application_id = "b12r8c72"
 end
 
 class ApplicationController < ActionController::Base; end
