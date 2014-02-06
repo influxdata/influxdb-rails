33bb43dde69d1deace35fdb72ee5bdfdf9c92264
Todd Persen <todd.persen@gmail.com>
2014-02-06 13:03:38 -0500

    Add more options to the initializer.

--- lib/rails/generators/influxdb/templates/initializer.rb
+++ lib/rails/generators/influxdb/templates/initializer.rb
@@ -4,4 +4,8 @@ InfluxDB::Rails.configure do |config|
   config.influxdb_password = "root"
   config.influxdb_host     = "localhost"
   config.influxdb_port     = 8086
+
+  # config.series_name_for_controller_runtimes = "rails.controller"
+  # config.series_name_for_view_runtimes       = "rails.view"
+  # config.series_name_for_db_runtimes         = "rails.db"
 end
