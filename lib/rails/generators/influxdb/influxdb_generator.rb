15db20fc28b1c5a3f2049c37c6bb257559d57cea
Todd Persen <todd.persen@gmail.com>
2014-02-06 10:28:26 -0500

    Clean up initializer naming.

--- lib/rails/generators/influxdb/influxdb_generator.rb
+++ lib/rails/generators/influxdb/influxdb_generator.rb
@@ -1,13 +1,12 @@
 require 'rails/generators'
 
 class InfluxdbGenerator < Rails::Generators::Base
-  desc "Description:\n  This creates a Rails initializer for InfluxDB."
+  desc "Description:\n  This creates a Rails initializer for InfluxDB::Rails."
 
-  puts "Creating things"
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_initializer_file
-    template "initializer.rb", "config/initializers/influxdb.rb"
+    template "initializer.rb", "config/initializers/influxdb-rails.rb"
   end
 
   def install
