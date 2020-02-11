f9ec74ce687ce7402ede5beabe32a517d58fb042
Christian Bruckmayer <christian@bruckmayer.net>
2020-02-11 22:36:31 +0000

    Implement save_and_open_metrics helper

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,18 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + "/test_client")
+require "launchy"
 
 module InfluxDB
   module Rails
     module Matchers
       def expect_metric(name: "rails", **options)
-        expect(TestClient.metrics).to include(
+        expect(metrics).to include(
           a_hash_including(options.merge(name: name))
         )
       end
 
       def expect_no_metric(name: "rails", **options)
-        expect(TestClient.metrics).not_to include(
+        expect(metrics).not_to include(
           a_hash_including(options.merge(name: name))
         )
+      end
+
+      def save_and_open_metrics
+        dir = File.join(File.dirname(__FILE__), "..", "..", "tmp")
+        FileUtils.mkdir_p(dir)
+        file_path = File.join(dir, "metrics.json")
+        output = JSON.pretty_generate(metrics)
+        File.write(file_path, output, mode: "wb")
+        ::Launchy.open(file_path)
+      end
+
+      private
+
+      def metrics
+        TestClient.metrics
       end
     end
   end
