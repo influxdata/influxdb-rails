32009ea1d205beea17e7dd76aa62d3d6d0e68ceb
Christian Bruckmayer <christian@bruckmayer.net>
2020-05-07 15:20:32 +0100

    Add specs for ActiveJob subscribers

--- lib/influxdb/rails/middleware/active_job_subscriber.rb
+++ lib/influxdb/rails/middleware/active_job_subscriber.rb
@@ -4,29 +4,10 @@ module InfluxDB
   module Rails
     module Middleware
       class ActiveJobSubscriber < Subscriber # :nodoc:
-        def short_hook_name
-          return "enqueue" if hook_name.include?("enqueue")
-          return "perform_start" if hook_name.include?("perform_start")
-          return "perform" if hook_name.include?("perform")
-        end
-
-        def job_state(payload)
-          return :failed if payload[:exception_object]
-
-          case short_hook_name
-          when "enqueue"
-            :queued
-          when "perform_start"
-            :running
-          when "perform"
-            :succeeded
-          end
-        end
-
         private
 
         def values(_start, duration, _payload)
-          value = measure_performace? ? duration : "1"
+          value = measure_performance? ? duration : 1
           {
             value: value,
           }
@@ -41,8 +22,27 @@ module InfluxDB
           }
         end
 
+        def short_hook_name
+          return "enqueue" if hook_name.include?("enqueue")
+          return "perform_start" if hook_name.include?("perform_start")
+          return "perform" if hook_name.include?("perform")
+        end
+
+        def job_state(payload)
+          return "failed" if payload[:exception_object]
+
+          case short_hook_name
+          when "enqueue"
+            "queued"
+          when "perform_start"
+            "running"
+          when "perform"
+            "succeeded"
+          end
+        end
+
         def measure_performance?
-          short_hook_name == "perform.active_job"
+          short_hook_name == "perform"
         end
       end
     end
---
+++ spec/requests/active_job_perform_start_metrics_spec.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+RSpec.describe "ActiveRecord metrics", type: :request do
+  let(:tags_middleware) do
+    lambda do |tags|
+      tags.merge(tags_middleware: :tags_middleware)
+    end
+  end
+  before do
+    allow_any_instance_of(InfluxDB::Rails::Configuration).to receive(:ignored_environments).and_return(%w[development])
+    allow_any_instance_of(ActionDispatch::Request).to receive(:request_id).and_return(:request_id)
+    allow_any_instance_of(InfluxDB::Rails::Configuration).to receive(:application_name).and_return(:app_name)
+    allow_any_instance_of(InfluxDB::Rails::Configuration).to receive(:tags_middleware).and_return(tags_middleware)
+  end
+
+  it "writes metric" do
+    get "/metrics"
+
+    expect_metric(
+      tags:   a_hash_including(
+        location:        :raw,
+        hook:            "perform_start",
+        state:           "running",
+        job:             "MetricJob",
+        queue:           "default",
+        server:          Socket.gethostname,
+        app_name:        :app_name,
+        tags_middleware: :tags_middleware
+      ),
+      values: a_hash_including(
+        value: 1
+      )
+    )
+  end
+
+  it "includes correct timestamps" do
+    travel_to Time.zone.local(2018, 1, 1, 9, 0, 0)
+
+    get "/metrics"
+
+    expect_metric(
+      tags:      a_hash_including(
+        location: :raw,
+        hook:     "perform_start"
+      ),
+      timestamp: 1_514_797_200
+    )
+  end
+
+  it "does not write metric when hook is ignored" do
+    allow_any_instance_of(InfluxDB::Rails::Configuration).to receive(:ignored_hooks).and_return(["perform_start.active_job"])
+
+    get "/metrics"
+
+    expect_no_metric(
+      tags: a_hash_including(
+        location: :raw,
+        hook:     "perform_start"
+      )
+    )
+  end
+end
